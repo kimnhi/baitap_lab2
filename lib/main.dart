f49dd464d5ffd8f215ea356867c6b1b9b4fb004a
kimnhi <nhivnk.21it@vku.udn.vn>
2024-08-28 16:35:03 +0700

    lab2

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text("I Am Rich",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.orange,
+            ),
+          ),
+          backgroundColor: Colors.black,
+          elevation: 0,
+          centerTitle: true,
+          // leading: Icon(Icons.arrow_back_ios),
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('lab2/assets/images/kimnhi.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
